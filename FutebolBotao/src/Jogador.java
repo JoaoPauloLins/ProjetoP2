ce4f9359c8444bbb4bb51a5d2e7a2214b867672e
Joao Paulo Lins de Oliveira <jplo@windows.cin.ufpe.br>
2017-06-08 18:46:16 -0300

    Projeto Futebol Botao

---
+++ FutebolBotao/src/Jogador.java
@@ -0,0 +1,55 @@
+
+public class Jogador {
+
+	public String nome;
+	public int numeroCamisa;
+	public boolean bola;
+	
+	//Construtor	
+	public Jogador (String nome, int numeroCamisa){
+		this.nome = nome;
+		this.numeroCamisa = numeroCamisa;
+	}
+	
+	//Get e Set
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getNumeroCamisa() {
+		return numeroCamisa;
+	}
+
+	public void setNumeroCamisa(int numeroCamisa) {
+		this.numeroCamisa = numeroCamisa;
+	}
+	
+	public boolean isBola() {
+		return bola;
+	}
+
+	public void setBola(boolean bola) {
+		this.bola = bola;
+	}
+	
+	//Métodos
+	
+	public void passeBola(Jogador companheiro){
+		if (bola){
+			companheiro.bola = true;
+			this.bola = false;
+		}
+	}
+	
+	public void roubarBola(Jogador adversario){
+		if (!bola){
+			this.bola = true;
+			adversario.bola = false;
+		}
+	}
+}
+                                                                                                                                                                                                                             
